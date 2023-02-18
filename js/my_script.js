3415207cbffb14e6f3d9b4d983ff85c0455ed5a5
dave89gray <david_gray89@aol.com>
2023-02-18 12:14:48 +0000

    finish challenge

--- js/my_script.js
+++ js/my_script.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-    $(h2).addClass("underline"); //underlines all <h2> elements
+    $("h2").addClass("underline"); //underlines all <h2> elements
     $("ul").addClass("border"); //adds border to all lists
 });
